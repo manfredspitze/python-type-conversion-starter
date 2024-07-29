dda7be6de4638229cb073ae3326e4aae4639e099
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-07-29 11:25:28 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+# Student name
+# Current date
+# Type Conversion in Python
+
+# Part 1
+# Comment
+# Comment
+
+
+
+
+# Part 2
+# Comment
+# Comment
+
+
+
+
+# Part 3
+# Comment
+# Comment
+
+
+
+
+# Part 4
+# Comment
+# Comment
+
+
+
+
+# Part 5
+# Comment
+# Comment
