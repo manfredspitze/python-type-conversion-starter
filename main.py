195fff51d58522715c43826cc78028a523878817
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-08-23 14:35:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,33 +3,42 @@
 # Type Conversion in Python
 
 # Part 1
-# Comment
-# Comment
+# Prompt the user to enter (input) their age as an integer
+# Then print a short sentence that tells the user how old they are
 
 
 
 
 # Part 2
-# Comment
-# Comment
+# Prompt the user to enter the price for an item at the grocery store
+# Make sure Python treats the user's input -- the price -- as a floating-point number
+# Then display the item's price in dollars
 
 
 
 
 # Part 3
-# Comment
-# Comment
+# Prompt the user to enter the length of a circle's radius (in feet)
+# Treat the radius as a floating-point number
+# Write the formula for calculating the area of a circle
+# HINT: The value of pi is 3.14159
+# Display a sentence that tells the user what the area of the circle is (in square feet)
 
 
 
 
 # Part 4
-# Comment
-# Comment
-
-
+# Prompt the user to enter the title of their favorite book or name of their favorite game
+# Use the Python upper ( ) method to convert the book title or name of the game to all uppercase characters
+# Print the uppercase version of the book title or name of the game
 
 
 # Part 5
-# Comment
-# Comment
+# Prompt the user to enter their last name
+# Use the built-in len ( ) function to get the number of characters in the user's last name
+# Print a short sentence that tells the user how many characters there are in his/her last name
+
+
+
+
+
